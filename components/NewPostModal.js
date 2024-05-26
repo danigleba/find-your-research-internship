abb437441e254fe298a953ae88223b4afae30813
Dani Gleba <daniglebapuig@gmail.com>
2024-05-26 16:53:10 +0200

    wip

--- components/NewPostModal.js
+++ components/NewPostModal.js
@@ -97,7 +97,7 @@ export default function NewPostModal({ user }) {
                 </div>      
                 <div className="form-section">
                   <label htmlFor="categories">Area</label>
-                  <div className="w-full border border-gray-300 px-3 rounded-lg" >
+                  <div className="w-full border border-gray-300 rounded-lg px-3 overflow-hidden" >
                     <select value={postData?.categories} onChange={(e) => handleChange(e, postData)} id="categories" name="categories" className="w-full h-12 focus:outline-none focus:ring-0">
                       {categories.map((item, index) => (
                         <option key={index} value={item}>{item}</option>
