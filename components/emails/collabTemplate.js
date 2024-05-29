6773272034586bb55321a8a277431a486b691c9b
Dani Gleba <daniglebapuig@gmail.com>
2024-05-29 09:11:00 +0200

    wup

--- components/emails/collabTemplate.js
+++ components/emails/collabTemplate.js
@@ -4,7 +4,7 @@ export default function collabTemplate({ author, user, title, comment }) {
       <p>Hi {user.name}</p>
       <p>Congrats!</p>
       <p><b>{author.name}</b>, a {author.position} from {author.institution}, <b>wants to collaborate</b> on your {title}.</p>
-      <p>You can <b>get it tough</b> with them by writing at {user.email}.</p>
+      <p>You can <b>get it touch</b> with them by writing at {user.email}.</p>
       {comment != "" && (<p>They also shared this message for you: "{comment}".</p>)}
       <p>Work together, make better science.</p>
       <p><i>Portiko Team</i></p>
