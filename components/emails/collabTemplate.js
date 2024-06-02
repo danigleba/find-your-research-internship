134a1b2007fe7df1e78f991ae75585c7026ea933
Dani Gleba <daniglebapuig@gmail.com>
2024-06-02 18:49:10 +0200

    wip

--- components/emails/collabTemplate.js
+++ components/emails/collabTemplate.js
@@ -1,9 +1,9 @@
 export default function collabTemplate({ author, user, title, comment }) {
   return (
     <div className="text-black"> 
-      <p>Hi {user.name}</p>
+      <p>Hi {author.name}</p>
       <p>Congrats!</p>
-      <p><b>{author.name}</b>, a {author.position} from {author.institution}, <b>wants to collaborate</b> on your {title}.</p>
+      <p><b>{user.name}</b>, a {user.position} from {user.institution}, <b>wants to collaborate</b> on your {title}.</p>
       <p>You can <b>get it touch</b> with them by writing at {user.email}.</p>
       {comment != "" && (<p>They also shared this message for you: "{comment}".</p>)}
       <p>Work together, make better science.</p>
