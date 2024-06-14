16f77350847c47eed28fb47528313da8109196e9
Dani Gleba <daniglebapuig@gmail.com>
2024-06-14 20:20:20 +0200

    wip

--- app/api/db/getProjects/route.js
+++ app/api/db/getProjects/route.js
@@ -1,11 +1,11 @@
 import { NextResponse } from "next/server"
-import supabase from "@/utils/supabase"
+import supabaseAdmin from "@/utils/supabaseAdmin"
 
 export async function POST(req) {
   const { page, section } = await req.json()
   const pageNumber = parseInt(page)
   try {
-    const { data: posts, error } = await supabase
+    const { data: posts, error } = await supabaseAdmin
       .from("projects")
       .select("*")
     return NextResponse.json({ data: posts })
