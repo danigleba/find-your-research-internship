af757b4125c5b461c515d90ede3f40bcfed6ef8c
Dani Gleba <daniglebapuig@gmail.com>
2024-06-15 17:41:05 +0200

    wip

--- components/NewPostModal.js
+++ components/NewPostModal.js
@@ -88,13 +88,8 @@ export default function NewPostModal({ user, getUserPosts, getUserProjects}) {
         <div className="modal-box space-y-6">
           <div className="modal-action">
             <form method="dialog" className="flex-box flex-col items-center w-full space-y-6" >
-              <p className="font-extrabold text-2xl text-center">Post a new technique you do</p>
+              <p className="font-extrabold text-2xl text-center">New Collaboration</p>
               <div className="w-full space-y-6">
-                <div className="form-section">
-                  <label htmlFor="Title">Name of the analysis, experiment or technique</label>
-                  <input value={postData?.title} onChange={(e) => handleChange(e, postData)} type="text" id="title" name="title" placeholder="Identification and characterization of microorganism " className="textarea textarea-bordered w-full focus:outline-none focus:ring-0"></input>
-                  {errors.title && <p className="error">{errors.title}</p>}
-                </div>
                 <div className="form-section">
                   <label htmlFor="Title">I have...</label>
                   <div className="flex-box flex-col items-start gap-2">
@@ -105,10 +100,15 @@ export default function NewPostModal({ user, getUserPosts, getUserProjects}) {
                     <div className="flex-box gap-3 text-sm font-medium"> 
                       <input type="radio" onChange={(e) => handleChange(e, postData)} checked={postData?.section == "projects"}  name="section" value="projects" id="projects" className="radio" />
                       <p>a project and I'm looking for collaborators</p>
-                    </div>
-                                      
+                    </div>          
                   </div>
                 </div>
+                <div className="form-section">
+                  <label htmlFor="Title">Name of the analysis, experiment or technique</label>
+                  <input value={postData?.title} onChange={(e) => handleChange(e, postData)} type="text" id="title" name="title" placeholder="Identification and characterization of microorganism " className="textarea textarea-bordered w-full focus:outline-none focus:ring-0"></input>
+                  {errors.title && <p className="error">{errors.title}</p>}
+                </div>
+                
                 <div className="form-section">
                   <label htmlFor="Description">Description</label>
                   <textarea value={postData?.description} onChange={(e) => handleChange(e, postData)} type="text" id="description" name="description" rows={3} className="textarea textarea-bordered w-full leading-6 focus:outline-none focus:ring-0" placeholder="Experiment the identification and characterization of microorganism using techniques like..."></textarea>
